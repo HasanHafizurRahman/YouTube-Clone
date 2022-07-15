ef6ef6e05721bcb7008e295b73cc1f17650323b7
Hasan Shanto <hasanshanto922@gmail.com>
2022-07-16 01:05:02 +0600

    mui icon

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import MyTube from '../img/logo.png';
+import HomeIcon from '@mui/icons-material/Home';
 
 const Container = styled.div`
 flex:1;
@@ -22,18 +23,30 @@ font-weight: bold;
 margin-bottom: 25px;
 `;
 
-const img = styled.img`
+const Img = styled.img`
 height: 25px;
 `;
+
+const Item = styled.div`
+display: flex;
+align-items: center;
+gap: 20px;
+cursor: pointer;
+`
+
 
 const Menu = () => {
     return (
         <Container>
             <Wrapper>
                 <Logo>
-                    <img src={MyTube}/>
+                    <Img src={MyTube}/>
                     MyTube
                 </Logo>
+                <Item>
+                    <HomeIcon />
+                    Home
+                </Item>
             </Wrapper>
         </Container>
        
